0ce3c8eb5a727b80ad33435fd29362ba98aa3791
xLexip <x@lexip.dev>
2021-12-31 11:35:05 +0100

    Include Google Camera 8.2.X.

--- common.mk
+++ common.mk
@@ -338,6 +338,17 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Doze
+PRODUCT_PACKAGES += \
+    OnePlusDoze
+
+# Prebuilts
+PRODUCT_PACKAGES += \
+    GoogleCamera
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilts/system/etc/permissions/org.codeaurora.snapcam.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/org.codeaurora.snapcam.xml
+
 # Privapp Whitelist
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
