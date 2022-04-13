0707bbe586fb7ae48a43d375070d8cdf76cf3256
Namkhai B <me@forkbomb9.ch>
2022-04-13 12:42:18 -0500

    msm8998-common: Import DeviceSettings from yaap
    - Imported from
      https://github.com/yaap-op5t/yaap_device_oneplus_msm8998-common/commit/23bd1ed82d08941d840dede378fe2694ab0ded3b
    
    Co-Authored-By: Ido Ben-Hur <idoybh2@gmail.com>
    Co-Authored-By: Anirudh Gupta <anirudhgupta109@gmail.com>
    Co-Authored-By: Anirudh Gupta <anirudhgupta109@aosip.dev>
    Co-Authored-By: David Sn <divad.nnamtdeis@gmail.com>
    Co-Authored-By: NurKeinNeid <mralexman3000@gmail.com>
    Co-Authored-By: Omkar Chandorkar <forumomkar@gmail.com>
    Co-Authored-By: enesykaya <enesykaya@gmail.com>
    Co-Authored-By: Adhitya Mohan <me@adhityamohan.in>
    Co-Authored-By: Daniel Solomon <solomondaniel@google.com>
    Co-Authored-By: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Co-Authored-By: LuK1337 <priv.luk@gmail.com>
    Co-Authored-By: Luca Stefani <luca.stefani.ge1@gmail.com>
    Co-Authored-By: Murtaza@Piplod <murtaza1926@gmail.com>
    Co-Authored-By: Nauval Rizky <enuma.alrizky@gmail.com>
    Co-Authored-By: Omkar Chandorkar <gotenksIN@aosip.dev>
    Co-Authored-By: SKULSHADY <anushekprasal@gmail.com>
    Co-Authored-By: ashwin4rc <ashwin2001achu@gmail.com>
    Co-Authored-By: itsjoeoui <itsjoeoui@pixelexperience.org>
    Co-Authored-By: maitreya29 <maitreyapatni30@gmail.com>
    Co-Authored-By: maxwen <max.weninger@gmail.com>
    Signed-off-by: Namkhai B <me@forkbomb9.ch>

--- common.mk
+++ common.mk
@@ -152,6 +152,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
+# Device Settings
+PRODUCT_PACKAGES += \
+    DeviceSettings
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
@@ -245,6 +249,7 @@ PRODUCT_PACKAGES += \
     init.ipastart.sh \
     init.qcom.rc \
     init.qcom.usb.rc \
+    init.target.displaymodes.rc \
     init.radio.sh \
     ueventd.qcom.rc
 
