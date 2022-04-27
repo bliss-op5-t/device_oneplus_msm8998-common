8a5c04c76aacbe81d1f671293f822d65e2c3a496
SirRGB <sirrgb@amyrom.ml>
2022-04-27 23:53:18 +0200

    msm8998-common: bringup bliss

--- common.mk
+++ common.mk
@@ -28,7 +28,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-yaap
+    $(LOCAL_PATH)/overlay-bliss
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
