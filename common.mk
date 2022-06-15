e21bb504c241b6ae34fa03ef78d0a773743843a0
lobo-1978 <104615878+lobo-1978@users.noreply.github.com>
2022-06-15 19:39:11 +1000

    Update common.mk

--- common.mk
+++ common.mk
@@ -38,7 +38,7 @@ TARGET_SUPPORTS_BLUR := true
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-bliss
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
